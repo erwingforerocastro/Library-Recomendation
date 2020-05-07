806c611d58c94dc014b7bdc9746fef5d46691ca5
Erwing Forero Castro <erwingforerocastro@gmail.com>
2020-05-07 16:05:39 -0500

    commit 07-05

---
+++ booksql-vue/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <ApolloQuery :query="require('@/graphql/queries/Categories.gql')">
+    <template slot-scope="{ result: { data, loading }, isLoading }">
+      <div v-if="isLoading">Loading...</div>
+      <ul v-else>
+        <li
+          v-for="category of data.categories"
+          class="user"
+          :key="category.id"
+        >{{ category.id }} {{category.name}}</li>
+      </ul>
+    </template>
+  </ApolloQuery>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      datos: null
+    };
+  }
+};
+</script>
